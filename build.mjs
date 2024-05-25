941040f9f5204c6af52b181579c26bbe5878a8f7
haitmt <taminhtienhai@gmail.com>
2024-05-26 00:16:22 +0700

    build: generate static pages at bundle phase

--- build.mjs
+++ build.mjs
@@ -1,19 +1,20 @@
 import { marked } from 'marked';
-import { readFileSync, readdirSync, unlinkSync, writeFileSync } from "node:fs";
+import { readFileSync, readdirSync, unlinkSync, writeFileSync, mkdirSync } from "node:fs";
 import path from 'node:path';
 
 const SRC_DIR = 'posts';
-const OUT_DIR = 'pages';
+const OUT_DIR = 'dist/pages';
+
+// create OUT_DIR if it doesn't existed
+mkdirSync(OUT_DIR);
 
 // cleanup OUT_DIR
-
 readdirSync(OUT_DIR).forEach(file => {
     let file_des = path.join(OUT_DIR, file);
     unlinkSync(file_des);
 });
 
 // read SRC_DIR and generate html to OUT_DIR
-
 readdirSync(SRC_DIR)
     .filter(f => f.endsWith('.md'))
     .forEach(f => {
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
 import { plugin as MDPlugin } from "vite-plugin-markdown";
-import { resolve } from 'path';
 
 export default defineConfig({
     base: '/',
@@ -12,14 +11,10 @@ export default defineConfig({
         }),
     ],
     build: {
-        target: 'esnext',
-        lib: {
-            entry: resolve(__dirname, 'src/app.ts'),
-        },
         rollupOptions: {
             external: [
                 /^posts/
             ],
-        }
+        },
     }
 });
