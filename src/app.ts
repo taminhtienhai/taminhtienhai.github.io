cbcb7e7d53cacb8b379a685cb8bfb1c03e5ed1ef
haitmt <taminhtienhai@gmail.com>
2024-07-21 14:43:15 +0700

    feat: support toggle between  light and dark mode

--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,16 @@ import 'uno.css';
 import { $Renderer } from './machine';
 
 $Renderer.start();
+
+
+
+document.getElementById('theme-switcher')?.addEventListener('click', (_) => {
+    if (document.documentElement.classList.contains('light')) {
+        document.documentElement.classList.remove('light');
+        document.documentElement.classList.add('dark');
+    }
+    else {
+        document.documentElement.classList.remove('dark');
+        document.documentElement.classList.add('light');
+    }
+})
