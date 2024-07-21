74059d275eb75560dc510f39a10f076dd48fb058
haitmt <taminhtienhai@gmail.com>
2024-07-21 17:54:54 +0700

    feat: dynamic load article list

--- tests/helper.spec.ts
+++ tests/helper.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { load_template_async } from '@src/helper';
+import { parse_json_into, load_static_json_text, load_template_async } from '@src/helper';
 
 test('ok test', () => {
     expect(true).toBe(true);
@@ -9,4 +9,12 @@ test('load static github resource', async () => {
     let content = await load_template_async({ input: { name: 'first_attempt' } });
 
     console.log(content);
+    expect(content).not.null;
+});
+
+test('load static json', async() => {
+    let text = load_static_json_text({ input: { name: 'blogs' } });
+    let blogs = await text.then(parse_json_into);
+    console.log(blogs);
+    expect(blogs).length.greaterThan(0);
 });
